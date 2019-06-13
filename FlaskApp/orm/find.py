0de9a31b0a1d2db8dd1c0352f387ef24f45c7e4c
Calvin B <calvin.bochulak@gmail.com>
2019-06-13 14:53:58 -0600

    Search for listening history by user_id and time range

--- FlaskApp/orm/find.py
+++ FlaskApp/orm/find.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 
-from FlaskApp.orm.util import update_user
 from FlaskApp import mongo
+from FlaskApp.orm.util import update_user
 
 
 def find_user(user_id):
@@ -11,3 +12,15 @@ def find_user(user_id):
 def find_playlist(playlist_id):
     return mongo.db.playlists.find_one({'playlist.id': playlist_id})
  
+
+def find_recently_played(user_id, start_time=None, end_time=None):
+    query = {'user.id': user_id}
+
+    if start_time or end_time:
+        query['played_at'] = {}
+        if start_time is not None:
+            query['played_at']['$gt'] = datetime.utcfromtimestamp(start_time)
+        if end_time is not None:
+            query['played_at']['$lt'] = datetime.utcfromtimestamp(end_time)
+
+    return [rp for rp in mongo.db.recently_played.find(query, projection)]
